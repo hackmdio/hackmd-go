640bee193647400379b22483be8b14180231aee4
Yukai Huang <yukaihuangtw@gmail.com>
2023-06-03 22:32:07 +0800

    feat: teams command

---
+++ cmd/hackmd-cli/cmd/teams.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/hackmdio/hackmd-go/hackmd-cli/internal"
+	"github.com/spf13/cobra"
+)
+
+// teamsCmd represents the teams command
+var teamsCmd = &cobra.Command{
+	Use:   "teams",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		
+		api := internal.GetHackMDClient()
+
+		teams, err := api.GetTeams()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, team := range teams {
+			fmt.Println(team.ID, team.Name, team.Path)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(teamsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// teamsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// teamsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
